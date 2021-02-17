8b2e39b682d1d77fb27dbe5e7d7fda25b1084150
e-frit <e.fritcher1990@gmail.com>
2021-02-17 15:00:52 -0800

    Week 5 setup

---
+++ M5_FRITCHER_ERIC/script.js
@@ -0,0 +1,23 @@
+// Part 1: Build Arrays
+
+// Create an array that includes four different colors as strings
+// Create an array with five different foods as strings
+// Create an array with six different animals as strings
+// Create an array with all even numbers from 0 – 20.
+
+// Part 2: Array Methods
+
+// Use the toString method to have all of the colors write to the HTML
+// Use the join method to have all of the colors write to the HTML with a separator
+// Use the pop method on the animals array and have the array log to the console
+// Use the push method to add a new animal to the animal array and have the array write to the HTML.
+// Use the splice method to add three new foods to the food array. Have this write to the HTML.
+// Use the slice method to move the 2nd, 3rd, and 4th foods into their own array. Have this write to the HTML.
+
+// Part 3: Array Sort and Iteration
+
+// Use the sort method on the numbers array. Remember you will need a compare function to do so. Have this log to the console.
+// Use the filter method on the numbers array to create a new array with all numbers higher than 10. Have this log to the console.
+// Use the find method on the numbers array to find the first element that is greater than 12. Have this log to the console.
+// Use the every method on the numbers array to check if the elements are greater than 8. Have this log to the console.
+// Use the some method on the numbers array to check if the elements are greater than 8. Have this log to the console.
