d6998744ed18a423ba97d477dfc7b163498e0d86
e-frit <e.fritcher1990@gmail.com>
2021-02-24 15:41:45 -0800

    Week 6 initial

---
+++ M6_FRITCHER_ERIC/script.js
@@ -0,0 +1,16 @@
+/* Create a function with an if else statement that will do the following:
+If the time is after 5am and before noon, a window alert says “Good Morning”
+If the time is between noon and 5pm, a window alert says “Good Afternoon”
+Otherwise, a window alert says “Good Night” */
+
+/* Create a switch that will do the following:
+Get the current day of the week
+Have a case for every day of the week, starting from 0 = “Sunday”. */
+
+// Have the day of the week write to the h2 in the HTML as the sentence “The current day is [DAY].”
+
+// Create a variable called “count” that is equal to the number 5.
+
+/* Create a while loop that checks if “count” is less than 20
+and then writes to the HTML the number in the paragraph with the id “counter”.
+Have each iteration of the loop increase “count” by 3. */
