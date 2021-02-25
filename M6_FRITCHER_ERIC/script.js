829588e93dca34eb978d15c1a8df5bb4dc45281d
e-frit <70717595+e-frit@users.noreply.github.com>
2021-02-25 09:52:34 -0800

    Part 3 partial

--- M6_FRITCHER_ERIC/script.js
+++ M6_FRITCHER_ERIC/script.js
@@ -3,34 +3,57 @@ If the time is after 5am and before noon, a window alert says “Good Morning”
 If the time is between noon and 5pm, a window alert says “Good Afternoon”
 Otherwise, a window alert says “Good Night” */
 (function (){
-    let d = new Date()
-    let h = d.getHours()
-    if (h >= 17) {alert("Good Night")}
-    else if (h >= 12) {alert("Good Afternoon")}
-    else if (h >= 0 && h <= 5) {alert("Good Morning")}
-})()
+    let h = new Date().getHours()
+    if (h >= 5 && h < 12)
+        {alert("Good Morning")}
+    else if (h >= 12 && h <= 17)
+        {alert("Good Afternoon")}
+    else {alert("Good Night")}
+})();
 
 /* Create a switch that will do the following:
 Get the current day of the week
 Have a case for every day of the week, starting from 0 = “Sunday”.
 Have the day of the week write to the h2 in the HTML as the sentence “The current day is [DAY].” */
-switch (day) {
-    case value_1:
-        statement_1;
+var day;
+switch (new Date().getDay()) {
+    case 0:
+        day = "Sunday"
+        console.log(day);
         break;
-    case value_2:
-        statement_2;
+    case 1:
+        day = "Monday"
+        console.log(day);
         break;
-    case value_3:
-        statement_3;
+    case 2:
+        day = "Tuesday"
+        console.log(day);
         break;
-    default:
-        default_statement;
-}
+    case 3:
+        day = "Wednesday"
+        console.log(day);
+        break;
+    case 4:
+        day = "Thursday"
+        console.log(day);
+        break;
+    case 5:
+        day = "Friday"
+        console.log(day);
+        break;
+    case 6:
+        day = "Saturday"
+        console.log(day);
+};
 
 // Create a variable called “count” that is equal to the number 5.
-var count = 5
+var count = 5;
 
 /* Create a while loop that checks if “count” is less than 20
 and then writes to the HTML the number in the paragraph with the id “counter”.
 Have each iteration of the loop increase “count” by 3. */
+do {
+    document.getElementById("counter").innerHTML = "The current iteration of count is " + count;
+    count += 3
+}
+while (count < 20) 
