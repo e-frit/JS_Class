5e333d4e613bc8f384307daae8abc009832695a6
e-frit <e.fritcher1990@gmail.com>
2021-03-03 17:03:43 -0800

    Week 7 initial upload

---
+++ M7_FRITCHER_ERIC/script.js
@@ -0,0 +1,32 @@
+/*For this assignment, you will create functions and invoke them using various event methods. 
+
+Setup
+
+Use the provided start files and create a script.js file.
+Connect your script file to the index file.
+Part 1: onload
+
+Create a function that will have a window alert that says “Welcome to my page!”
+Have this function run when the page loads. */
+
+
+/*  Part 2: onmouse
+
+Create a function that will remove the class “surprise” from the image. */
+
+//Create a function that will replace the class on the submit button to “active-btn”.
+
+/* Have the first function run when the image is moused over.
+Have the second function run when the mouse is pressed on the submit button.
+Part 3: onclick */
+
+// Create a function that will have a window alert that says “Confirm message submission”
+
+//Create a function that will have a window alert that says “Have a nice day”
+
+/* Have the first function run when the form submit button is clicked.
+Have the second function run when the image is double clicked.
+Submission
+
+Zip your file and submit the zipped file named M7_LASTNAME_FIRSTNAME.
+If you used any external resources to complete this assignment, please also submit a resource document. If you did not, leave a comment saying no external resources were used. */
