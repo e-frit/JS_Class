3e77e050e511eab1fcfb3ef70e218a447026d54c
e-frit <e.fritcher1990@gmail.com>
2021-03-06 18:37:26 -0800

    Assignment complete

--- M7_FRITCHER_ERIC/script.js
+++ M7_FRITCHER_ERIC/script.js
@@ -1,32 +1,43 @@
-/*For this assignment, you will create functions and invoke them using various event methods. 
-
-Setup
-
-Use the provided start files and create a script.js file.
-Connect your script file to the index file.
-Part 1: onload
-
-Create a function that will have a window alert that says “Welcome to my page!”
+/*Create a function that will have a window alert that says “Welcome to my page!”
 Have this function run when the page loads. */
+(function() {
+    alert("Welcome to my page!")
+})();
 
+// Create a function that will remove the class “surprise” from the image. */
+function noSuprises() {
+    var a = document.querySelector("img.surprise");
+    a.removeAttribute('class')
+};
 
-/*  Part 2: onmouse
+// Create a function that will replace the class on the submit button to “active-btn”.
+function becomeActiveBtn() {
+    var a = document.querySelector("input.passive-btn");
+    a.removeAttribute('class');
+    a.setAttribute('class', 'active-btn')
+};
 
-Create a function that will remove the class “surprise” from the image. */
+// Have the first function run when the image is moused over.
+var imgNode = document.querySelector("img.surprise");
+imgNode.addEventListener('mouseenter', noSuprises);
 
-//Create a function that will replace the class on the submit button to “active-btn”.
+// Have the second function run when the mouse is pressed on the submit button.
+var btn = document.querySelector("input.passive-btn");
+btn.addEventListener('click', becomeActiveBtn);
 
-/* Have the first function run when the image is moused over.
-Have the second function run when the mouse is pressed on the submit button.
-Part 3: onclick */
+// Create a function that will have a window alert that says “Confirm message submission” */
 
-// Create a function that will have a window alert that says “Confirm message submission”
+function userConfirmation() {
+    alert("Confirm message submission")
+};
 
-//Create a function that will have a window alert that says “Have a nice day”
+//   Create a function that will have a window alert that says “Have a nice day”
+function closingSalutation() {
+    alert("Have a nice day")
+};
 
-/* Have the first function run when the form submit button is clicked.
-Have the second function run when the image is double clicked.
-Submission
+// Have the first function run when the form submit button is clicked
+btn.addEventListener('click', userConfirmation);
 
-Zip your file and submit the zipped file named M7_LASTNAME_FIRSTNAME.
-If you used any external resources to complete this assignment, please also submit a resource document. If you did not, leave a comment saying no external resources were used. */
+// Have the second function run when the image is double clicked
+imgNode.addEventListener('dblclick', closingSalutation)
